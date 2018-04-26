0297a3fa94a4676c8f883b9f128beca63c7fd6e6
J.S Eriksson <jonna.eriksson@bioenv.gu.se>
2018-04-26 14:10:32 +0200

    Update wgd_mapping.sh

--- scripts/wgd_mapping.sh
+++ scripts/wgd_mapping.sh
@@ -3,7 +3,7 @@
 echo "Start mapping"
 #$1 is the taxon´s name
 
-clc_mapper -o $1_$2.cas -p fb ss 300 1000 -q (PATH TO TRIMMED FILES) -s .07 -d $3 --cpus 1;
+clc_mapper -o $1_$2.cas -p fb ss 300 1000 -q {4} -s .07 -d $3 --cpus 1;
 echo "Mapping completed, starting cas to bam conversion"
 
 clc_cas_to_sam -a $1_$2.cas -o $1_$2.bam -f 33 -u;
